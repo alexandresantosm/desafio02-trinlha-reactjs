7d96f6b3320c56343d290f012078f80075552bad
Alexandre Menezes <amenezes031@gmail.com>
2021-03-21 19:49:19 -0300

    :recycle: Content component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,25 @@
-export function Content() {
-  // Complete aqui
+import { Header } from "./Header";
+import { ListMovies } from "./ListMovies";
+
+import '../styles/content.scss';
+
+interface ContentProps {
+  selectedGenreId: number;
+}
+
+export function Content({ selectedGenreId }: ContentProps) {
+
+  return (
+    <div className="container">
+      <Header 
+        selectedGenreId={selectedGenreId}
+      />
+
+      <main>
+        <ListMovies 
+          selectedGenreId={selectedGenreId}
+        />
+      </main>
+    </div>
+  );
 }
